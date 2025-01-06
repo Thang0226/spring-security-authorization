496eafe5320fe800b0654762a06914312ba61d79
Thang0226 <thang.nd0226@gmail.com>
2025-01-06 10:17:53 +0700

    configured spring security & authorized user

---
+++ src/main/java/com/controller/SecurityController.java
@@ -0,0 +1,29 @@
+package com.controller;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecurityController {
+    private String getPrincipal() {
+        String userName;
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principal instanceof UserDetails) {
+            userName = ((UserDetails) principal).getUsername();
+        } else {
+            userName = principal.toString();
+        }
+
+        return userName;
+    }
+
+    @GetMapping(value = {"/", "/home"})
+    public String homepage(Model model) {
+        model.addAttribute("user", getPrincipal());
+        return "welcome";
+    }
+}
